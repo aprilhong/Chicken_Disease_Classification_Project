df55631ea1e936693bad257afb29ab0a6bef71b7
April Hong <78663820+aprilhong@users.noreply.github.com>
2024-07-15 11:10:12 -0400

    add exception

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import sys
 from ClassifierProj.logger import logging
+from ClassifierProj.exception import CustomException
 
+# Run to test logger.py
 logging.info("Welcome to my custom classifier!")
+
+# Run to test exception.py
+try:
+    a=1/0
+except Exception as e:
+        logging.info('Divide by Zero')
+        raise CustomException(e,sys)
